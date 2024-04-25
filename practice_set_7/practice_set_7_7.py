44b3f68f6f57d3430f28e1ba981e0040cc5fdce0
honey8899 <amansrivastava8778@gmail.com>
2024-04-25 13:22:40 +0530

    modified one file

--- practice_set_7/practice_set_7_7.py
+++ practice_set_7/practice_set_7_7.py
@@ -4,7 +4,7 @@
 #  *****
 # *******
 
-n= int(input("Enter the no of rows"))
+n= int(input("Enter the no of rows: "))
 for i in range(n):  
     print(" "*(n-i-1),end="")
     print("*"*(2*i+1),end="")    # end="" enables us to skip the new line which comes after the print() by default.
